a1a29a5b8b8d117de29672189fba5659994d0165
Claytone Curth <claytonecurth@gmail.com>
2026-04-13 23:58:44 +0300

    Revert to CommonJS for better Vercel compatibility

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,6 @@
 const allowedOrigins = ['*'];
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', allowedOrigins.join(','));
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
